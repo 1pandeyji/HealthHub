bd61ab71ac5dbb38849d6115ebb9378726ee553b
Yogesh Kumar <89840329+1pandeyji@users.noreply.github.com>
2024-01-17 22:27:20 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -12,7 +12,6 @@ function performSearch () {
   const searchInput = document.getElementById('searchInput')
   const searchTerm = searchInput.value.toLowerCase()
 
-  // Perform your search logic here, you can filter or manipulate content based on the search term.
-  // For demonstration, let's just log the search term to the console.
+ 
   console.log('Search Term:', searchTerm)
 }
